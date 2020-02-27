d7bcde20e5f9d3f80e14fdf9698fcdb1433a267d
ohsorry <ohsorry@local>
2020-02-27 15:34:43 +0800

    server loader

---
+++ Shadowsocks-Net/Minimal/Shadowsocks-Minimal-CrossPlatform-Local/DefaultServerLoader.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Shadowsocks-Net https://github.com/shadowsocks/Shadowsocks-Net
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Buffers;
+using System.Linq;
+using System.IO;
+using Microsoft.Extensions.Logging;
+using Argument.Check;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Shadowsocks_Minimal_Crossplatform_Local
+{
+    using Shadowsocks;
+    using Shadowsocks.Local;
+    using System.Net;
+    using System.Text.Encodings.Web;
+    using System.Text.Unicode;
+
+    public class DefaultServerLoader : IServerLoader
+    {
+        List<Server> _servers = null;
+        object _readwriteLock = new object();
+
+        public DefaultServerLoader()
+        {
+        }
+
+        public Server Load(EndPoint target = null)
+        {
+            if (null == _servers || 0 == _servers.Count)
+            {
+                lock (_readwriteLock)
+                {
+                    //var options = new JsonSerializerOptions
+                    //{
+                    //    WriteIndented = true,
+                    //    IgnoreReadOnlyProperties = true,
+                    //    IgnoreNullValues = true,
+                    //    ReadCommentHandling = JsonCommentHandling.Skip,
+                    //    AllowTrailingCommas = true,
+                    //    //PropertyNameCaseInsensitive = false;
+                    //    //Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)//, (UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                    //};
+                    //options.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+                    try
+                    {
+                        _servers = JsonSerializer.Deserialize<List<Server>>(File.ReadAllText("servers.json"));
+                    }
+                    catch { return null; }
+                }
+            }
+            if (_servers.Count > 0)
+            {
+                return _servers[0];
+            }
+            return null;
+        }
+    }
+}
