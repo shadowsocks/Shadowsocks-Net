72f376200dabb81ba11c5e4bbc117663c475ca0b
ohsorry <ohsorry@local>
2020-02-28 02:14:49 +0800

    test

--- Shadowsocks-Net/Linux/Shadowsocks-Linux-Local/Program.cs
+++ Shadowsocks-Net/Linux/Shadowsocks-Linux-Local/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿/*
+ * Shadowsocks-Net https://github.com/shadowsocks/Shadowsocks-Net
+ */
 
-namespace Shadowsocks_Linux
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Buffers;
+using System.IO;
+using Argument.Check;
+
+namespace Shadowsocks_Linux_Remote
 {
+    using Shadowsocks;
+    using Shadowsocks.Local;
+    using Shadowsocks.Infrastructure;
+    using Shadowsocks.Infrastructure.Sockets;
+
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+
+            await Task.CompletedTask;
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
--- Shadowsocks-Net/Linux/Shadowsocks-Linux-Remote/Program.cs
+++ Shadowsocks-Net/Linux/Shadowsocks-Linux-Remote/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿/*
+ * Shadowsocks-Net https://github.com/shadowsocks/Shadowsocks-Net
+ */
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Buffers;
+using System.IO;
+using Argument.Check;
+
 
 namespace Shadowsocks_Linux_Remote
 {
+    using Shadowsocks;
+    using Shadowsocks.Local;
+    using Shadowsocks.Infrastructure;
+    using Shadowsocks.Infrastructure.Sockets;
+
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await Task.CompletedTask;
+            Console.WriteLine("Hello World! exit");
+            Console.ReadKey();
         }
     }
 }
