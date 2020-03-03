ec29dde2d65c50a2ddbf6597731a7d9b0dbaaa06
ohsorry <ohsorry@local>
2020-03-04 00:44:44 +0800

    rename

---
+++ Shadowsocks-Net/Shadowsocks/Local/UdpEncapsulationFilter.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * Shadowsocks-Net https://github.com/shadowsocks/Shadowsocks-Net
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Argument.Check;
+
+namespace Shadowsocks.Local
+{
+    using Infrastructure;
+    using Infrastructure.Pipe;
+    using Infrastructure.Sockets;
+
+
+    /*
+    # SOCKS5 UDP Request
+    # +----+------+------+----------+----------+----------+
+    # |RSV | FRAG | ATYP | DST.ADDR | DST.PORT |   DATA   |
+    # +----+------+------+----------+----------+----------+
+    # | 2  |  1   |  1   | Variable |    2     | Variable |
+    # +----+------+------+----------+----------+----------+
+
+    # SOCKS5 UDP Response
+    # +----+------+------+----------+----------+----------+
+    # |RSV | FRAG | ATYP | DST.ADDR | DST.PORT |   DATA   |
+    # +----+------+------+----------+----------+----------+
+    # | 2  |  1   |  1   | Variable |    2     | Variable |
+    # +----+------+------+----------+----------+----------+
+
+                # shadowsocks UDP Request (before encrypted)
+                # +------+----------+----------+----------+
+                # | ATYP | DST.ADDR | DST.PORT |   DATA   |
+                # +------+----------+----------+----------+
+                # |  1   | Variable |    2     | Variable |
+                # +------+----------+----------+----------+
+
+                # shadowsocks UDP Response (before encrypted)
+                # +------+----------+----------+----------+
+                # | ATYP | DST.ADDR | DST.PORT |   DATA   |
+                # +------+----------+----------+----------+
+                # |  1   | Variable |    2     | Variable |
+                # +------+----------+----------+----------+
+     */
+    /// <summary>
+    /// Applies to UDP relay client on local side.
+    /// </summary>
+    public class UdpEncapsulationFilter : PipeFilter
+    {
+        public UdpEncapsulationFilter(IClient udpClient, ILogger logger = null)
+               : base(udpClient, PipeFilterCategory.Encapsulation, 0, logger)
+        {
+        }
+
+        public override PipeFilterResult AfterReading(PipeFilterContext ctx)
+        {
+            if (!ctx.Memory.IsEmpty)
+            {
+                SmartBuffer toApplication = SmartBuffer.Rent(1500);
+                ctx.Memory.CopyTo(toApplication.Memory.Slice(2 + 1));
+                var p = toApplication.Memory.Span;
+                p.Slice(0, 3).Fill(0x0);
+                toApplication.SignificantLength = ctx.Memory.Length + 2 + 1;
+                return new PipeFilterResult(ctx.Client, toApplication, true);
+            }
+            else { _logger?.LogError($"LocalUdpRelayPackingFilter AfterReading filterContext.Memory.IsEmpty"); }
+
+            return new PipeFilterResult(this.Client, null, false);
+        }
+
+        public override PipeFilterResult BeforeWriting(PipeFilterContext ctx)
+        {
+            if (!ctx.Memory.IsEmpty)
+            {
+                SmartBuffer toRemote = SmartBuffer.Rent(1500);
+                ctx.Memory.Slice(3).CopyTo(toRemote.Memory);
+                toRemote.SignificantLength = ctx.Memory.Length - 3;
+                return new PipeFilterResult(ctx.Client, toRemote, true);
+            }
+            else { _logger?.LogError($"LocalUdpRelayPackingFilter BeforeWriting filterContext.Memory.IsEmpty"); }
+
+            return new PipeFilterResult(this.Client, null, false);
+        }
+    }
+}
---
+++ Shadowsocks-Net/Shadowsocks/Remote/UdpRelayEncapsulationFilter.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * Shadowsocks-Net https://github.com/shadowsocks/Shadowsocks-Net
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Argument.Check;
+
+namespace Shadowsocks.Remote
+{
+    using Infrastructure;
+    using Infrastructure.Pipe;
+    using Infrastructure.Sockets;
+    /*
+              
+                # shadowsocks UDP Request (before encrypted)
+                # +------+----------+----------+----------+
+                # | ATYP | DST.ADDR | DST.PORT |   DATA   |
+                # +------+----------+----------+----------+
+                # |  1   | Variable |    2     | Variable |
+                # +------+----------+----------+----------+
+
+                # shadowsocks UDP Response (before encrypted)
+                # +------+----------+----------+----------+
+                # | ATYP | DST.ADDR | DST.PORT |   DATA   |
+                # +------+----------+----------+----------+
+                # |  1   | Variable |    2     | Variable |
+                # +------+----------+----------+----------+    
+     */
+    /// <summary>
+    /// Applies to UDP relay client.
+    /// </summary>
+    class UdpRelayEncapsulationFilter : PipeFilter
+    {
+
+        public UdpRelayEncapsulationFilter(IClient udpClient, ILogger logger = null)
+               : base(udpClient, PipeFilterCategory.Encapsulation, 0, logger)
+        {
+        }
+        public override PipeFilterResult BeforeWriting(PipeFilterContext ctx)
+        {
+            SmartBuffer toTarget = SmartBuffer.Rent(ctx.Memory.Length);
+            if (ShadowsocksAddress.TryResolveLength(ctx.Memory, out int targetAddrLen))
+            {
+                ctx.Memory.Slice(targetAddrLen).CopyTo(toTarget.Memory);
+                toTarget.SignificantLength = ctx.Memory.Length - targetAddrLen;
+                return new PipeFilterResult(ctx.Client, toTarget, true);
+            }
+            else
+            {
+                return new PipeFilterResult(ctx.Client, toTarget, false);
+            }
+        }
+
+        public override PipeFilterResult AfterReading(PipeFilterContext ctx)
+        {
+            SmartBuffer toSsLocal = SmartBuffer.Rent(1500);//TODO what if exceeds 1500? fragments or not?
+
+            if (ShadowsocksAddress.TrySerailizeTo(
+                               (byte)(AddressFamily.InterNetworkV6 == ctx.Client.EndPoint.AddressFamily ? 0x4 : 0x1),
+                               ctx.Client.EndPoint.Address.GetAddressBytes(),
+                               (ushort)ctx.Client.EndPoint.Port,
+                               toSsLocal.Memory,
+                               out int written))
+            {
+                toSsLocal.SignificantLength = written;
+                int payloadToCopy = Math.Min(toSsLocal.FreeSpace, ctx.Memory.Length);
+                ctx.Memory.Slice(0, payloadToCopy).CopyTo(toSsLocal.FreeMemory);
+                toSsLocal.SignificantLength += payloadToCopy;
+
+                return new PipeFilterResult(ctx.Client, toSsLocal, true); ;
+            }
+
+            return new PipeFilterResult(ctx.Client, null, false); ;
+        }
+
+
+    }
+}
