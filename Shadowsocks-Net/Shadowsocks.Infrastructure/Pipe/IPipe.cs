f3dd7a2ecf039c43bd463f1504544d43e5652e1e
ohsorry <ohsorry@local>
2020-04-11 14:32:22 +0800

    refator

---
+++ Shadowsocks-Net/Shadowsocks.Infrastructure/Pipe/IPipe.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Shadowsocks-Net https://github.com/shadowsocks/Shadowsocks-Net
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Shadowsocks.Infrastructure.Pipe
+{
+    public interface IPipe
+    {
+        IClientReader Reader { get; }
+        IClientWriter Writer { get; }
+    }
+}
---
+++ Shadowsocks-Net/Shadowsocks.Infrastructure/Pipe/SimplexPipe.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Shadowsocks-Net https://github.com/shadowsocks/Shadowsocks-Net
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Shadowsocks.Infrastructure.Pipe
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SimplexPipe : IPipe
+    {
+        public IClientReader Reader => throw new NotImplementedException();
+
+        public IClientWriter Writer => throw new NotImplementedException();
+
+        public virtual void Pipe()
+        {
+
+        }
+    }
+}
