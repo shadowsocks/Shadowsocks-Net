b34c258039b6c7c0477dfa47e74d277e6d3cdbaf
ohsorry <ohsorry@local>
2020-04-15 09:57:13 +0800

    save

---
+++ Shadowsocks-Net/Shadowsocks/Tunnel/ConnectionPool`1.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Shadowsocks-Net https://github.com/shadowsocks/Shadowsocks-Net
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Buffers;
+using System.Linq;
+using System.IO;
+using Microsoft.Extensions.Logging;
+using Argument.Check;
+using System.Text;
+
+namespace Shadowsocks.Tunnel
+{
+    using Infrastructure;
+    using Infrastructure.Sockets;
+    using Infrastructure.Pipe;
+    public class ConnectionPool<TClient>
+         where TClient : IClient
+    {
+    }
+}
